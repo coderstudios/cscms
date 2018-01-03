dc63356c56a9a3e977c190b5c0a66417f29a45e1
David Wright <dave@coderstudios.com>
2018-01-03 19:26:44 +0000

    Add new classes to adjust table column sizing based on content

--- resources/views/backend/pages/article.blade.php
+++ resources/views/backend/pages/article.blade.php
@@ -36,13 +36,13 @@ Articles
                 @if (count($vars['articles']))
                     @foreach($vars['articles'] as $item)
                     <tr>
-                        <td>{{ $item->title }}</td>
-                        <td>{{ $item->slug }}</td>
+                        <td class="text-truncate">{{ $item->title }}</td>
+                        <td class="text-truncate">{{ $item->slug }}</td>
                         <td>{{ $item->type->name }}</td>
                         <td>{{ (strlen($item->publish_at)) ? $item->publish_at->format('d/m/Y H:i:s') : '' }}</td>
                         <td>{{ $item->enabled ===1 ? 'Yes' : 'No' }}</td>
                         <td>{{ $item->sort_order }}</td>
-                        <td>
+                        <td class="text-nowrap">
                             <a href="{{ route('backend.articles.article.edit' , ['id' => !empty($item->latest_revision_id) ? $item->latest_revision_id : $item->id ]) }}" class="btn btn-sm btn-primary">Edit</a>
                             <a onclick="event.preventDefault(); if(confirm('Are you sure you want to delete this article?')){document.getElementById('delete-form').setAttribute('action',this.href); document.getElementById('delete-form').submit();}" href="{{ route('backend.articles.article.delete' , ['id' => !empty($item->latest_revision_id) ? $item->latest_revision_id : $item->id ]) }}" class="btn btn-sm btn-danger">Delete</a>
                             </td>
